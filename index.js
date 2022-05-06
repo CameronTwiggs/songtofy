29b8211c3222321231e849a036de7df8d8fe9a36
CameronTwiggs <cameron.020617@gmail.com>
2022-05-06 14:51:53 -0400

    feature:updateList() for songs

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const songItem = document.querySelector(".song_item")
-console.log(songItem)
+const songs = ["Easy On Me","Stay","Heat Waves","Shivers","Industry","Cold Heart","Bad Habbits"]
+const list = document.querySelector(".song-list")
+
+function updateSongs(songs,element) {
+    let result = "";
+    for (let song of songs) {
+        result += `<li class="song-item">${song} <button>Move Up</button> <button>Move Down</button> <button>Remove</button></li>`
+    }
+    element.innerHTML = result
+}
+
+
+updateSongs(songs,list)
